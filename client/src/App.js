65834730dfbf0cc73336be6d3bd566b92fa8bbc2
Hyun Sung Cho <hcho774@gmail.com>
2022-10-15 14:50:25 -0400

    set up routes to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Home from "./pages/home/Home";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Profile from "./pages/profile/Profile";
+import MissingPerson from "./pages/missingperson/MissingPerson";
+import SafeLocation from "./pages/safelocation/SafeLocation";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/dashboard" element={<Dashboard />}></Route>
+        <Route exact path="/profile" element={<Profile />}></Route>
+        <Route exact path="/missingperson" element={<MissingPerson />}></Route>
+        <Route exact path="/safelocation" element={<SafeLocation />}></Route>
+      </Routes>
     </div>
   );
 }
