09409febf562da77ddbe45e48a4361b0be2acb27
James Coffman <jamesc4023@gmail.com>
2022-10-15 18:20:45 -0400

    css stuff

--- client/src/App.js
+++ client/src/App.js
@@ -12,33 +12,40 @@ import GetLocation from "./components/GetLocation.jsx";
 function App() {
   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    getUser();
-  }, []);
+  /*useEffect(() => {*/
+  /*getUser();*/
+  /*}, []);*/
 
-  //getting user information from our database by using useEffect and Fetching
-  const getUser = () =>
-    //custom route /me checks if user is currently logged in
-    fetch("/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user)); // if user is logged in then set user informtion to userstate
-      }
-    });
+  /*//getting user information from our database by using useEffect and Fetching*/
+  /*const getUser = async () =>*/
+  /*//custom route /me checks if user is currently logged in*/
+  /*// let req = await fetch("/validate-couple");*/
+  /*let req = await fetch("/me")*/
+  /*if (req.ok) {*/
+  /*setUser(req.json())*/
+  /*}*/
+  /*});*/
 
   // checks if user is not logged in then redirect to Homepage that has login modal
-  if (!user) return <Home />;
+  // if (!user) return <Home />;
   // user={user} setUser={setUser} navigate={navigate}
   return (
     <div className="App">
       <NavBar />
-      <GetLocation />
-      <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/dashboard" element={<Dashboard />}></Route>
-        <Route exact path="/profile" element={<Profile />}></Route>
-        <Route exact path="/missingperson" element={<MissingPerson />}></Route>
-        <Route exact path="/safelocation" element={<SafeLocation />}></Route>
-      </Routes>
+      <div className="container">
+        <GetLocation />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/dashboard" element={<Dashboard />}></Route>
+          <Route exact path="/profile" element={<Profile />}></Route>
+          <Route
+            exact
+            path="/missingperson"
+            element={<MissingPerson />}
+          ></Route>
+          <Route exact path="/safelocation" element={<SafeLocation />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
