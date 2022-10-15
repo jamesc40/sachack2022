7e46cf55823fae7891aef8669a47c150af5f7282
Eric Xiao <es.xiao93@gmail.com>
2022-10-15 16:02:01 -0400

    added missingPersonData table, controller, model, route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :missing_person_data
   resources :users, only: [:show, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
