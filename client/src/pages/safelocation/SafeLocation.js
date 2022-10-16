c85c57902e856802bdfe5e979e34074bba0848c4
jss475 <shin.seung.won@gmail.com>
2022-10-15 21:43:51 -0400

    safe locations folder updated

--- client/src/pages/safelocation/SafeLocation.js
+++ client/src/pages/safelocation/SafeLocation.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Weather from "./Weather";
+import News from "./News";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const SafeLocation = () => {
-  return <div>SafeLocation</div>;
+  return (
+    <div>
+      <h1>Safe Location Page</h1>
+      <Container>
+        <Row>
+        <Col>
+          <Weather />
+        </Col>
+        
+        <Col>
+          <News />
+        </Col>
+        </Row>
+      </Container>
+    </div>
+  )
 };
 
 export default SafeLocation;
