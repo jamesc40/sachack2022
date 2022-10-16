95c71cb576a4b7e20e9558051f5bfa2fda596e17
Eric Xiao <es.xiao93@gmail.com>
2022-10-16 09:50:26 -0400

    added scrollbar to news feed

--- client/src/pages/safelocation/NewsCard.js
+++ client/src/pages/safelocation/NewsCard.js
@@ -3,13 +3,40 @@ import Stack from 'react-bootstrap/Stack';
 import Card from 'react-bootstrap/Card';
 
 function NewsCard() {
+
+  const url = "rPniJbwGLKW68npVL5K0QfKCC5EbD3Q0"
+
   return (
-    <Stack>
-      <Card style={{ width: '30rem' }}>
+    <Stack className="ml-2">
+      <Card style={{ width: '30rem' }} >
         <Card.Body>
           <Card.Title>Incoming Hurricane</Card.Title>
           <Card.Text>
             Hide your kids, hide your wife!
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '30rem' }}>
+        <Card.Body>
+          <Card.Title>Flood Alert</Card.Title>
+          <Card.Text>
+            Hide your car!
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '30rem' }}>
+        <Card.Body>
+          <Card.Title>Flood Alert</Card.Title>
+          <Card.Text>
+            Hide your car!
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '30rem' }}>
+        <Card.Body>
+          <Card.Title>Flood Alert</Card.Title>
+          <Card.Text>
+            Hide your car!
           </Card.Text>
         </Card.Body>
       </Card>
