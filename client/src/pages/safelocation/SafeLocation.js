5dfa9654839b6f3acf0153d67db533a653610d5f
jss475 <shin.seung.won@gmail.com>
2022-10-15 21:19:59 -0400

    Revert "working on weather news"
    This reverts commit eeddde7bae0002fb5e1884b3ef9da3286e58bb00.

--- client/src/pages/safelocation/SafeLocation.js
+++ client/src/pages/safelocation/SafeLocation.js
@@ -1,21 +1,7 @@
 import React from "react";
-import Weather from "./Weather";
-import News from "./News";
 
 const SafeLocation = () => {
-  return (
-    <div>
-      <h1>Safe Location Page</h1>
-      {/* <div class="row justify-content-around">
-        <div class="col-4">
-          <Weather />
-        </div>
-        <div class="col-4">
-          <News />
-        </div>
-      </div> */}
-    </div>
-  )
+  return <div>SafeLocation</div>;
 };
 
 export default SafeLocation;
